9df41fc3fc5114e44cf292ad525f08240bff9262
simon1938 <simonillouzlaurent@gmail.com>
2022-11-21 17:31:55 +0100

    création de la grille 3*3 avec tkinters

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 
-
 from tkinter import *
 
-window = Tk()
-window.geometry("720x480")
-window.title("bienvenue au jeu du force3")
-window.config(background='grey')
-label_tile=Label(window ,text="Bienvenue au jeu du force 3",font=("Courriel",20),bg='blue',fg='white')
-label_tile.pack()
+class canva(Canvas):
+
+    def __init__(self):
+
+        # Variables
+
+        self.cases = []  # Cases déjà remplies
+        self.listerouge = []  # Liste des cases rouges
+        self.listejaune = []  # Liste des cases jaunes
+        self.fonce = "navy blue"
 
 
-window.mainloop()
+        self.canva = Canvas.__init__(self, width=165*3, height=165*3, bg=self.fonce, bd=0)
+        self.grid(row=1, columnspan=5)
+
+
+        # Création des cases
+
+        self.ovals = []
+        for y in range(10, 165*3, 55*3):
+            for x in range(10, 165*3, 55*3):
+                self.ovals.append(self.create_rectangle(x, y, x + 50*3, y + 50*3, fill="white"))
+
+
+if __name__ == "__main__":
+    window = Tk()
+    window.title("Puissance 4")
+    window.config(bg="navy blue")
+    canva = canva()
+    window.mainloop()
