7af7dde8f299957cb055e45668fe32b4aa2f6532
simon1938 <simonillouzlaurent@gmail.com>
2022-12-07 23:58:13 +0100

    correction du bug

--- NewMinmax.py
+++ NewMinmax.py
@@ -14,7 +14,7 @@ def evaluate(board : GameArea):
             return -10
         else:
             return 0
-
+#défine a list of two dimentions (list of coordinates) for where the token can be moved
 def allmovecirculartokens(board):
     tab = [[]]
     for col in range(3):
@@ -22,33 +22,38 @@ def allmovecirculartokens(board):
             if (board.gamearea[row][col].squaretoken != None):
                 if (board.gamearea[row][col].squaretoken.circletoken == None):
                     tab.append([row, col])
+    #remove the first element of the list
     tab.pop(0)
 
     return tab
-
+#
 def get_possible_moves(board,ismax):
 
     allmove= move()
     allmove.place_token=[[]]
     allmove.move_tokens=allmovecirculartokens(board)
+    allmove.place_token=allmovecirculartokens(board)
 
-    for col in range(3):
-        for row in range(3):
-             if(board.gamearea[row][col].squaretoken!=None):
-                if(board.gamearea[row][col].squaretoken.circletoken==None):
-                   allmove.place_token.append([row,col])
+    # for col in range(3):
+    #     for row in range(3):
+    #          if(board.gamearea[row][col].squaretoken!=None):
+    #             if(board.gamearea[row][col].squaretoken.circletoken==None):
+    #                allmove.place_token.append([row,col])
     allmove.place_token.pop(0)
 
 
     return allmove
 
 def make_move(board,litlemove,indexmove,ismax):
+
+    #just for checking errors
     if(board==None):
         print("board is none")
 
-
+    #the idexmove is to select the type of move(0=place token,1=move token,2=move square 3 move 2square)
     if(indexmove==0):
         if(ismax):
+            #litlemove is a list of two dimentions (list of coordinates) for where the token can be moved
             board.addCircleToken(litlemove[0],litlemove[1],board.player_1)
             board.displayGameArea()
         else:
@@ -63,28 +68,29 @@ def make_move(board,litlemove,indexmove,ismax):
 
 def minmax(state, depth, ismax):
 
+   #evaluate the board
    score = evaluate(state)
    if(score==10):
         return score
    if(score==-10):
         return score
+   #To add a end of the tree
    if(depth==0):
         return 0
-   print("retour mini"+str(depth))
 
    listofmove =get_possible_moves(state, 1).place_token
-
 
    if ismax:
             bestValue = -5000
             for move in listofmove:
-                print(listofmove)#le probleme est ici car la liste de move devient recursivement infinie
+                print(listofmove)
                 state.displayGameArea()
                 #copy du board
                 newState = deepcopy(state)
                 #on joue le coup
                 make_move(newState, move, 0, 1)
                 print("deeep="+str(depth)+"player 1"+str(move))
+                #on appelle minmax sur le nouveau board
                 value = minmax(newState, depth - 1, False)
                 bestValue = max(bestValue, value)
 
@@ -100,12 +106,15 @@ def minmax(state, depth, ismax):
           #on joue le coup
           make_move(newState, move, 0, 0)
           print("deeep tour ="+str(depth)+"player 2"+str(move))
+          #on appelle minmax sur le nouveau board
           value = minmax(newState, depth - 1, True)
           bestValue = min(bestValue, value)
 
         return bestValue
 
-
+#permets de trouver le meilleur coup en utilisant minimax
+#évalu de tous les coups possibles au premier tour avec minimax
+#et renvois juste les coodonnées du meilleur coup
 def findBestMove(board):
     bestVal = -1000
     bestMove = (-1, -1)
@@ -131,9 +140,9 @@ if __name__ == '__main__':
     player_1 = Player(0, "R")
     player_2 = Player(1, "B")
     board = GameArea(player_1, player_2)
-    board.addCircleToken(2, 0, player_2)
-    board.addCircleToken(2, 1, player_2)
-    board.addCircleToken(0, 2, player_1)
+    board.addCircleToken(2, 0, player_1)
+    board.addCircleToken(2, 1, player_1)
+    board.addCircleToken(0, 2, player_2)
     board.displayGameArea()
     print("la valeur maximal est "+str(minmax(board, 4,True)))
     findBestMove(board)
